3df55d3ca848a031671fdea6fa5debba01ea9c95
Richard Adams <richard@researchspace.com>
2017-02-20 21:08:23 +0000

    License

---
+++ src/main/java/com/researchspace/figshare/model/License.java
@@ -0,0 +1,18 @@
+package com.researchspace.figshare.model;
+
+import java.net.URL;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class License {
+	
+	URL url;
+	String name;
+	Integer value;
+
+}
